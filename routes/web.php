f920a3ac37fa3a54ce713859727de5436a691613
Hardik Chauhan <hardik.chauhan@bacancytechnology.com>
2019-09-24 18:56:04 +0530

    Updated new routes

--- routes/web.php
+++ routes/web.php
@@ -54,7 +54,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Auth::routes();
-Route::group(['prefix'=>'','middleware'=>['auth']], function(){
+Route::group(['prefix'=>'admin','middleware'=>['auth']], function(){
 
   Route::get('/home', 'HomeController@index')->name('home');
   Route::get('/home1', 'HomeController@index1')->name('home1');
