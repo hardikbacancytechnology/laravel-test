4f6b87d30e01836c9417dff95e274384c85b82ae
Hardik Chauhan <hardik.chauhan@bacancytechnology.com>
2019-09-26 16:37:33 +0530

    Development changes - 26092019

--- routes/web.php
+++ routes/web.php
@@ -58,8 +58,8 @@ Route::group(['prefix'=>'admin','middleware'=>['auth']], function(){
   Route::get('/home', 'HomeController@index')->name('home');
   Route::get('/home1', 'HomeController@index1')->name('home1');
   Route::get('/profile', 'UserController@profile')->name('profile');
-  Route::resource('users', 'UserController');
-  Route::post('/users/listings', 'UserController@listings');
   Route::get('/users/change-password', 'UserController@changePassword');
   Route::post('/users/change-password', 'UserController@storeNewPassword');
+  Route::post('/users/listings', 'UserController@listings');
+  Route::resource('users', 'UserController');
 });
