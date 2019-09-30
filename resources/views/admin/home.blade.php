930c6dcbabed91b7c403f8b6cf112a5b5814f912
Hardik Chauhan <hardik.chauhan@bacancytechnology.com>
2019-09-30 12:58:35 +0530

    Development changes - 30092019

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -19,7 +19,7 @@
 	<!-- /.col -->
 	<div class="col-md-3 col-sm-6 col-xs-12">
 		<div class="info-box">
-			<span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
+			<span class="info-box-icon bg-red"><i class="fab fa-google-plus-g"></i></span>
 			<div class="info-box-content">
 				<span class="info-box-text">Likes</span>
 				<span class="info-box-number">41,410</span>
