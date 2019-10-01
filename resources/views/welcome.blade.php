ca4973e1aa9ac179b285b7e447e68a440d2ab3bd
Hardik Chauhan <hardik.chauhan@bacancytechnology.com>
2019-10-01 10:08:03 +0530

    Development changes - 01102019

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,7 +72,7 @@ Intro Section
 ============================-->
 <section id="intro">
     <div class="intro-container">
-        <div id="introCarousel" class="carousel  slide carousel-fade" data-ride="carousel">
+        <div id="introCarousel" class="carousel slide carousel-slide" data-ride="carousel">
             <ol class="carousel-indicators"></ol>
             <div class="carousel-inner" role="listbox">
                 <div class="carousel-item active">
