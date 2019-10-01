801d182426fd1ca290d52058b80b049ba5eff6cd
Hardik Chauhan <hardik.chauhan@bacancytechnology.com>
2019-10-01 12:21:43 +0530

    Development changes - 01102019

--- vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/layout.blade.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/layout.blade.php
@@ -4,12 +4,9 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>@yield('title')</title>
-
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
         <!-- Styles -->
         <style>
             html, body {
@@ -20,25 +17,20 @@
                 height: 100vh;
                 margin: 0;
             }
-
             .full-height {
                 height: 100vh;
             }
-
             .flex-center {
                 align-items: center;
                 display: flex;
                 justify-content: center;
             }
-
             .position-ref {
                 position: relative;
             }
-
             .content {
                 text-align: center;
             }
-
             .title {
                 font-size: 36px;
                 padding: 20px;
